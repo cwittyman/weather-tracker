ea325f94f6ac63c3cb0c657187bac0025f9b58af
Chase Witt <cwitty@Chases-MacBook-Pro.local>
2022-11-28 03:11:18 -0500

    Change to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_23_214344) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "forecasts", force: :cascade do |t|
     t.string "temp"
     t.string "mintemp"
@@ -22,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_214344) do
     t.string "timezone"
     t.string "utc_offset"
     t.string "type", default: "HourlyForecast", null: false
-    t.integer "location_id", null: false
+    t.bigint "location_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["location_id"], name: "index_forecasts_on_location_id"
